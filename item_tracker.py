57da9ac982e9ebc3779c4f2b394a3c86efec3fac
Steve Michelini <sdmichelini@gmail.com>
2015-07-12 16:05:56 -0400

    Added an API backend. Also added CSS and JS parts. You can now see static JSON displayed. Also reconfigured the app.yaml file to handle the static files.

--- item_tracker.py
+++ item_tracker.py
@@ -1,6 +1,7 @@
 import os
 import urllib
 import webapp2
+import json
 
 # Send's back the static HTML for the Index Page
 class StaticIndexHandler(webapp2.RequestHandler):
@@ -8,6 +9,7 @@ class StaticIndexHandler(webapp2.RequestHandler):
     	f = open('static/index.html')
     	self.response.headers['Content-Type'] = 'text/html'
         self.response.write(f.read())
+        f.close()
 
 # Send's back the static HTML for the Items Page
 class StaticCreateItemHandler(webapp2.RequestHandler):
@@ -15,7 +17,19 @@ class StaticCreateItemHandler(webapp2.RequestHandler):
 		self.response.headers['Content-Type'] = 'text/plain'
 		self.response.write('Items Go Here')
 
+
+class ApiItemsHandler(webapp2.RequestHandler):
+	def get(self):
+		#TODO: Implement Database instead of Static Data
+		data = [{'id':0,'name':'Bleach', 'status':'Awaiting Approval', 'price':12.0, 'link':'google.com', 'reason_needed':'Clean the Bathrooms'}]
+		data_string = json.dumps(data, sort_keys=True, indent=2)
+		self.response.headers['Content-Type'] = 'application/json'
+		self.response.write(data_string)
+
+
+
 app = webapp2.WSGIApplication([
     ('/', StaticIndexHandler),
-    ('/items',StaticCreateItemHandler)
+    ('/items',StaticCreateItemHandler),
+    ('/api/items',ApiItemsHandler)
 ], debug=True)
