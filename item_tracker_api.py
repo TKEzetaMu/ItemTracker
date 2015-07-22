b6397757f41da6b87f64d3ff418ea8f75bdb77d9
Steve Michelini <sdmichelini@gmail.com>
2015-07-21 22:11:14 -0400

    Changed the backend to support CRU operations. Still need to work on Delete.
    Did a lot of work on Frontend in ReactJS and CSS. Added a Material Design

--- item_tracker_api.py
+++ item_tracker_api.py
@@ -6,15 +6,23 @@ import endpoints
 
 from google.appengine.ext import ndb
 from protorpc import remote
+from protorpc import messages
+from protorpc import message_types
 
-from endpoints_proto_datastore.ndb import EndpointsModel
+# Remove all Proto Datastore Code
+#from endpoints_proto_datastore.ndb import EndpointsModel
 
-# Basic Definition of an Item
-class Item(EndpointsModel):
+
+# NDB Model
+# Used to store the item in NDB. Note the required properties
+class ItemModel(ndb.Model):
     # Name of the Item
     name = ndb.StringProperty(required = True)
     # Reason it is Required
     reason = ndb.StringProperty(required = True)
+
+    # Link to the resource
+    link = ndb.StringProperty()
     # Date Requested
     date = ndb.DateTimeProperty(auto_now_add = True)
     # Did the Cryso Approve It?
@@ -24,17 +32,100 @@ class Item(EndpointsModel):
     # Status of the Request
     status = ndb.IntegerProperty()
 
+# This is the request and the response when you make an item
+class MakeItemRequestResponse(messages.Message):
+    # Here we are only requested the two required properties and an optional link. The next ones can be inferred or default
+
+    # See NDB model 'name'
+    name = messages.StringField(1, required = True)
+    # See NDB Model 'reason'
+    reason = messages.StringField(2, required = True)
+    # Optional Link(Url)
+    link = messages.StringField(3)
+    #only used for response
+    urlsafe = messages.StringField(4)
+
+class ItemResponse(messages.Message):
+    # Here we are only requested the two required properties and an optional link. The next ones can be inferred or default
+
+    # See NDB model 'name'
+    name = messages.StringField(1, required = True)
+    # See NDB Model 'reason'
+    reason = messages.StringField(2, required = True)
+    # Optional Link(Url)
+    link = messages.StringField(3)
+    #only used for response
+    urlsafe = messages.StringField(4)
+    # Date Requested
+    date = messages.StringField(5)
+    # Did the Cryso Approve It?
+    cryso_approved = messages.BooleanField(6)
+    # Did the Pylo Approve It?
+    pylo_approved = messages.BooleanField(7)
+    # Status of the Request
+    status = messages.IntegerField(8)
+
+# List of items
+class ItemResponseList(messages.Message):
+    items = messages.MessageField(ItemResponse, 1, repeated = True)
+
+# This is what allows someone to change an item
+class ChangeItemRequestResponse(messages.Message):
+    # NDB id
+    identifier = messages.StringField(1, required = True)
+    # Did the Cryso Approve It?
+    cryso_approved = messages.BooleanField(2)
+    # Did the Pylo Approve It?
+    pylo_approved = messages.BooleanField(3)
+    # Status of the Request
+    status = messages.IntegerField(4)
+
 @endpoints.api(name = 'items_api', version='v1', description='API for the Item Tracker')
 class ItemsApi(remote.Service):
 
-    @Item.method(path='item', http_method='POST', name='item.insert')
-    def ItemInsert(self, item_):
-        item_.put()
-        return item_
+    @endpoints.method(MakeItemRequestResponse, ItemResponse,
+    path='item', http_method='POST',
+    name='items.make')
+    def make_item(self, request):
+        item = ItemModel(name = request.name, reason = request.reason, link = request.link, cryso_approved = False, pylo_approved = False, status = 0)
+        item.put()
+        response = ItemResponse(name = item.name, reason = item.reason, link = item.link, cryso_approved = item.cryso_approved, pylo_approved = item.pylo_approved, status = 0, urlsafe = item.key.urlsafe(), date = item.date.isoformat())
+        return response
 
-    @Item.query_method(path='items', name='item.list')
-    def ItemList(self, query):
-        return query
+    @endpoints.method(ChangeItemRequestResponse, ChangeItemRequestResponse,
+    path='item/change', http_method = 'PUT',
+    name='items.update')
+    def update_item(self,request):
+        item_key = ndb.Key(urlsafe=request.identifier)
+        item = item_key.get()
+        if request.cryso_approved == True:
+            item.cryso_approved = True
+        else:
+            request.cryso_approved = item.cryso_approved
+        if request.pylo_approved == True:
+            item.pylo_approved = True
+        else:
+            request.pylo_approved = item.pylo_approved
+        item.put()
+        return request
+
+    @endpoints.method(message_types.VoidMessage, ItemResponseList,
+    path='items', http_method='GET',
+    name = 'items.list')
+    def get_items(self,request):
+        items_ = []
+        for item in ItemModel.query():
+            items_.append(ItemResponse(name = item.name, reason = item.reason, link = item.link, cryso_approved = item.cryso_approved, pylo_approved = item.pylo_approved, status = 0, urlsafe = item.key.urlsafe(), date = item.date.isoformat()))
+        return ItemResponseList(items = items_)
+
+    # @Item.method(path='item', http_method='POST', name='item.insert')
+    # def ItemInsert(self, item_):
+    #     item_.put()
+    #     return item_
+    #
+    # @Item.query_method(path='items', name='item.list')
+    # def ItemList(self, query):
+    #     return query
 
 
 
--- static/js/app.js
+++ static/js/app.js
@@ -1,8 +1,50 @@
 var Item = React.createClass({
+  getStatusMessage: function(status){
+    var message = '';
+    switch(Number(status)){
+      case 0:
+        message = 'Awaiting Approval';
+        break;
+      case 1:
+        message = 'Approved. Awaiiting Purchase'
+        break;
+      case 2:
+        message = 'Closed';
+        break;
+      case 3:
+        message = 'Request Denied'
+        break;
+      default:
+        message = 'Unknown'
+        break;
+    }
+    return <span id='status_message' className='hidden-xs'>{message}</span>
+  },
+  createName: function(name){
+    return <span className="itemName">{name}</span>;
+  },
+  sanitizeLink: function(link){
+    return <a href={link}>Link</a>;
+  },
+  createDate: function(date){
+    var monthNames = ["January", "February", "March", "April", "May", "June",
+  "July", "August", "September", "October", "November", "December"
+];
+    var d = new Date(Date.parse(date));
+    return monthNames[d.getMonth()] + " " + d.getDay()
+  },
+  createDateAndLink: function(date, link){
+    var monthNames = ["January", "February", "March", "April", "May", "June",
+  "July", "August", "September", "October", "November", "December"
+];
+    var d = new Date(Date.parse(date));
+    return <div id="date">{this.createDate(date)} | <a href={link}>Link</a></div>;
+  },
   render: function(){
     return (
       <div className='item'>
-        {this.props.name}
+        {this.createName(this.props.name)} {this.getStatusMessage(this.props.status)}{this.createDateAndLink(this.props.date, this.props.link)}<br/>
+        <div className='reason'>{this.props.reason}</div>
       </div>
     );
   }
@@ -11,20 +53,49 @@ var Item = React.createClass({
 var ItemList = React.createClass({
   getInitialState: function(){
     return {
-      
+      items:[]
     };
   },
+  loadItemsFromServer: function(){
+    $.ajax({
+      url: '/_ah/api/items_api/v1/items',
+      dataType: 'json',
+      cache: false,
+      success: function(data) {
+        this.setState({items: data.items});
+      }.bind(this),
+      error: function(xhr, status, err) {
+        console.error(this.props.url, status, err.toString());
+      }.bind(this)
+    });
+  },
+  componentDidMount: function() {
+    this.loadItemsFromServer();
+  },
+  render: function(){
+    var itemNodes = this.state.items.map(function(item){
+      return (
+        <Item name={item.name} status={item.status} date={item.date} reason={item.reason} urlsafe={item.urlsafe} link={item.link}/>
+      );
+    });
+    return (
+      <div className='items'>
+        {itemNodes}
+      </div>
+    );
+
+  }
 });
 
 var CommentBox = React.createClass({
   render: function() {
     return (
         <div className="commentBox">
-          <Item name='Bleach'/>
+          <Item name='Bleach' status='Awating Approval' date='Some Radnom Time' reason='Do I need one?'/>
         </div>
     );
     }
   });
   React.render(
-    <CommentBox />,	document.getElementById('content')
+    <ItemList />,	document.getElementById('content')
 );
